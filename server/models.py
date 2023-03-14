561b8ea3811c4711fb3201ae998103a2c6cf7a99
Ziru Zou <jiyucoding@yahoo.com>
2023-03-14 17:09:01 -0400

    display data from related models using SerializerMixin

--- server/models.py
+++ server/models.py
@@ -1,8 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Game(db.Model):
+# 1. pass SerializerMixin to the model that has 1-m relationship
+# it adds a to_dict() instance method to the Game model
+class Game(db.Model, SerializerMixin):
     __tablename__ = 'games'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -13,12 +16,16 @@ class Game(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    # a game has many reviews: 1-m relationship between game and reviews 
     reviews = db.relationship('Review', backref='game')
+
+    # 2. configure with SerializerMixin
+    serialize_rules = ('-reviews.game', )
 
     def __repr__(self):
         return f'<Game {self.title} for {self.platform}>'
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -30,10 +37,12 @@ class Review(db.Model):
     game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    serialize_rules = ('-game.reviews', '-user.reviews', )
+
     def __repr__(self):
         return f'<Review ({self.id}) of {self.game}: {self.score}/10>'
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -42,4 +51,7 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    # a user has many reviews: 1-m relationship between user and reviews 
     reviews = db.relationship('Review', backref='user')
+
+    serialize_rules = ('-reviews.user', )
